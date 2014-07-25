960f65baf44a0e90acbb64f9ffe5c1d7ec503b5c
Yusuke Miyazaki <miyazaki.dev@gmail.com>
2014-07-26 02:07:12 +0900

    Update README.md

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ except:
 
 
 setup(name='python-gyazo',
-      version='0.1.0',
+      version='0.2.0',
       description='A Python wrapper for Gyazo API',
       author='Yusuke Miyazaki',
       author_email='miyazaki.dev@gmail.com',
